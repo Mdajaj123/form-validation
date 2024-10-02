ac70d67a4eacb334d06ac9f30cab833d8e5b34de
unknown <mdijaz2596@gmail.com>
2024-10-02 17:10:12 +0530

    changes

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,16 +1,16 @@
-import React,{useState} from 'react'
-import {useNavigate } from 'react-router-dom';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 const Login = () => {
     const navigate = useNavigate();
     const [username, setUsername] = useState({
         password: '',
-        confirmPassword:""
+        confirmPassword: ""
     });
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setUsername({ ...username, [name]: value });
     }
-    const handleSubmit = (e) => { 
+    const handleSubmit = (e) => {
         e.preventDefault();
         const userData = JSON.parse(localStorage.getItem("user"));
 
@@ -20,31 +20,31 @@ const Login = () => {
         }
         else if (username.password !== userData.password || username.confirmPassword !== userData.confirmPassword) {
             alert("Pls enter valid credential");
-           
+
         }
     }
-  return (
-          <div className='d-flex justify-content-center align-items-center '>
-              <div className='w-50 shadow p-4'>
-                  <form onSubmit={handleSubmit}>
-                      <div className="mb-3">
-                          <label htmlFor="password" className="form-label">Password</label>
-                          <input type="password" className="form-control" id="password" name="password" value={username.password} onChange={handleInputChange} />
-                      </div>
-                      <div className="mb-3">
-                          <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
-                          <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={username.confirmPassword} onChange={handleInputChange} />
-                      </div>
-                      <div className="mb-3 form-check">
-                          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                      </div>
-                      <button type="submit" className="btn btn-primary">Submit</button>
-                  </form>
-              </div>
-          </div>
+    return (
+        <div className='d-flex justify-content-center align-items-center '>
+            <div className='w-50 shadow p-4'>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" name="password" value={username.password} onChange={handleInputChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+                        <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={username.confirmPassword} onChange={handleInputChange} />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
 
-  )
+    )
 }
 
 export default Login
